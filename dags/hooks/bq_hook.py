48a9114415939ab1478eb9631bcc5810eb0bccb3
Your Name <you@example.com>
2019-11-21 00:16:42 -0500

    ...

--- dags/hooks/bq_hook.py
+++ dags/hooks/bq_hook.py
@@ -531,7 +531,7 @@ class BigQueryBaseCursor(object):
                     time.sleep(5)
                 else:
                     raise Exception(
-                        'BigQuery job status check failed. Final error was: {} - {} - {}'.format(err.resp.status,err.resp.reason,err.resp.uri))
+                        'BigQuery job status check failed. Final error was: {} - {} '.format(err.resp.status,err.resp.reason))
 
         return job_id
 
